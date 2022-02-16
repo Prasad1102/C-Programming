9f9588a3f04bdacfdfc9d34daa0b8f262b938927
Prasad Shelke <shelkeprasad1102@gmail.com>
2022-02-16 14:40:23 +0530

    Hello world

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  printf("Hello world\n");
+}
+
+/*
+
+OUTPUT
+
+[prasad@localhost C-Programming]$ cc hello_world.c
+[prasad@localhost C-Programming]$ ./a.out
+Hello world
+[prasad@localhost C-Programming]$ 
+*/
